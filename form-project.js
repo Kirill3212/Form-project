e6787cfc884fc80c91e8a9d63169665fc3bbbf35
Superman <kirill.terekhin.98@bk.ru>
2023-08-28 11:37:59 +0300

    adding lower input that shows some info about user or admin

--- form-project.js
+++ form-project.js
@@ -3,10 +3,13 @@ const nameInput = document.getElementById("name");
 const emailInput = document.getElementById("email");
 const dateInput = document.getElementById("date");
 const aboutInput = document.getElementById("about");
+const submitBtn = document.getElementById("submitBtn");
+const userData = document.querySelector(".user-data");
+const userDataInput = document.querySelector(".user-data-input");
 
+// show/hide spinner/fieldSet
 const spinner = document.getElementById("spinner");
 spinner.style.display = "block";
-
 const fieldSet = document.querySelector("fieldset");
 fieldSet.style.display = "none";
 
@@ -29,6 +32,7 @@ setTimeout(function () {
       aboutInput.value = data[selector.selectedIndex].profile.about;
       console.log(data);
     });
+  // hide/show spinner/fieldSet
   spinner.style.display = "none";
   fieldSet.style.display = "block";
 }, 1000);
@@ -43,4 +47,68 @@ selector.addEventListener("change", function () {
   dateInput.value = data[selector.selectedIndex].profile.dob;
   emailInput.value = data[selector.selectedIndex].email;
   aboutInput.value = data[selector.selectedIndex].profile.about;
+});
+
+// Showing brief info with submit button
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // show user data
+  userData.style.display = "block";
+  userDataInput.innerHTML = `${
+    data[selector.selectedIndex].profile.name
+  }'s adress is ${data[selector.selectedIndex].profile.address}`;
+
+  // checking if the person is admin
+  if (data[selector.selectedIndex].roles.includes("admin")) {
+    let id = Array.from(data[selector.selectedIndex].id);
+    let numbers = "";
+
+    for (let i = 0; i < id.length; i++) {
+      if (Number(id[i]) || Number(id[i]) == 0) numbers += id[i];
+      else if (!Number(id[i]) && Number(id[i - 1])) {
+        switch (id[i]) {
+          case "a":
+            numbers += "-10--";
+            break;
+          case "b":
+            numbers += "-11--";
+            break;
+          case "c":
+            numbers += "-12--";
+            break;
+          case "d":
+            numbers += "-13--";
+            break;
+          case "e":
+            numbers += "-14--";
+            break;
+          case "f":
+            numbers += "-15--";
+            break;
+        }
+      }
+    }
+
+    let numbersPair = numbers.split("--");
+    let numbersPairArrays = numbersPair.map((pair) => {
+      return pair.split("-");
+    });
+    const resultPin = numbersPairArrays.map((pair) => {
+      if (!pair[1]) {
+        return Number(pair[0]) + 100;
+      } else return +pair[0] + +pair[1];
+    });
+    const resultNumber = resultPin.reduce((acc, n) => {
+      return (acc += n);
+    });
+    console.log(resultNumber);
+    // end of checking if the person is admin
+
+    // show admin data
+    userData.style.display = "block";
+    userDataInput.innerHTML = `The admin's pincode from ${
+      data[selector.selectedIndex].id
+    } is ${resultNumber}`;
+  }
 });
